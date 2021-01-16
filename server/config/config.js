f075d70abda8dace2045b57b972bdcb9568ad605
Carlos Castro <castrocarlos313@gmail.com>
2021-01-16 14:46:05 -0300

    Login normal seccion 10

--- server/config/config.js
+++ server/config/config.js
@@ -9,6 +9,18 @@ process.env.PORT = process.env.PORT || 3000;
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
+///============================
+///     Vencimiento del Token
+///============================
+
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+
+///============================
+///     SEED de autentificacion
+///============================
+
+process.env.SEED = 'este-es-el-seed-desarrollo';
 
 let urlDB;
 
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const _ = require('underscore');
+
 const Usuarios = require('../models/usuario');
+const { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion');
+
 const app = express();
 
 
-app.get('/usuario', function(req, res) {
+app.get('/usuario', verificaToken, (req, res) => {
+
+    return res.json({
+        usuario: req.usuario,
+        nombre: req.usuario.nombre,
+        email: req.usuario.email
+    });
 
     let desde = req.query.desde || 0;
     desde = Number(desde);
@@ -36,7 +45,7 @@ app.get('/usuario', function(req, res) {
 
 });
 
-app.post('/usuario', function(req, res) {
+app.post('/usuario', [verificaToken, verificaAdmin_Role], (req, res) => {
     let body = req.body;
 
     let usuario = new Usuarios({
@@ -65,7 +74,7 @@ app.post('/usuario', function(req, res) {
     });
 });
 
-app.put('/usuario/:id', function(req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
     Usuarios.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
@@ -87,10 +96,10 @@ app.put('/usuario/:id', function(req, res) {
         });*/
 });
 
-app.delete('/usuario/:id', function(req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
     let id = req.params.id;
 
-    Usuarios.findByIdAndUpdate(id, { estado: false }, { new: true }, (err, usuarioDB) => {
+    Usuarios.findByIdAndUpdate(id, { estado: false }, { new: true, useFindAndModify: false }, (err, usuarioDB) => {
         if (err) {
             return res.status(400).json({
                 ok: false,
